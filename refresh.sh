8886205cbcb926e8f6c00513078e57c450905402
Yangmun Choi <yangmungi@ucla.edu>
2026-03-12 20:07:57 -0700

    Update refresh

--- refresh.sh
+++ refresh.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-NODE_VERSION=${NODE_VERSION:-23}
 NPM_VERSION=${NPM_VERSION:-11.6.0}
 
-docker pull node:$NODE_VERSION
+docker pull node:23
 
-for subdirectory in claude-code openai-codex opencode-ai
+for subdirectory in openai-codex opencode-ai
 do
         (
                 cd $subdirectory
@@ -13,10 +12,24 @@ do
         )
 done
 
+docker pull "debian:trixie"
+
+for subdirectory in claude-code ai-cli
+do
+        (
+                cd $subdirectory
+                docker build --progress plain -t ymdc/$subdirectory .
+        )
+done
+
+docker pull "python:3.12"
+
 (
         cd marimo
         docker build --progress plain -t ymdc/marimo .
 )
+
+# probably best to just check for condaforge/miniforge3
 
 (
         cd jupyter
