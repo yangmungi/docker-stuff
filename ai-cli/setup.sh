73ac9c6e7183791afb4739607ba89cb42c813b61
Yangmun Choi <yangmungi@ucla.edu>
2026-03-12 20:07:48 -0700

    Add multi-AI client image.

---
+++ ai-cli/setup.sh
@@ -0,0 +1,6 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+curl -fsSL https://claude.ai/install.sh | bash
+
+#/bin/bash -c "source /root/.nvm/nvm.sh && nvm install 25 && nvm use 25"
+#/bin/bash -c "source /root/.nvm/nvm.sh && npm install -g npm"
+#/bin/bash -c "source /root/.nvm/nvm.sh && npm install -g @openai/codex opencode-ai"
